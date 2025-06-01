2cfc48e125f665aafe3077296d665b850a20281a
Vipul <145755437+vipul2231020@users.noreply.github.com>
2025-06-02 00:57:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,5 +44,5 @@ def chat():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Get PORT from environment, default to 5000
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
